68ad37081ee1b7bf529257f2db327b815c835238
zadelacerda <zephren123@gmail.com>
2020-01-29 17:01:56 -0800

    Initial commit

---
+++ app/src/main/java/edu/wwu/csci412/multipoll/Friends.java
@@ -0,0 +1,20 @@
+package edu.wwu.csci412.multipoll;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class Friends extends Fragment {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/edu/wwu/csci412/multipoll/Groups.java
@@ -0,0 +1,15 @@
+package edu.wwu.csci412.multipoll;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Groups extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups);
+
+
+    }
+}
