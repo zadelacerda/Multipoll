a941d1ffd3fced152cb389bdf6bc72503c1ade22
Zephren de la Cerda <zephren123@gmail.com>
2020-03-05 13:39:23 -0800

    push before database linking

--- app/src/main/java/edu/wwu/csci412/multipoll/Controller/ChooseElements.java
+++ app/src/main/java/edu/wwu/csci412/multipoll/Controller/ChooseElements.java
@@ -2,24 +2,32 @@ package edu.wwu.csci412.multipoll.Controller;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.SearchView;
+import android.widget.*;
+
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
 
 import javax.security.auth.callback.Callback;
 
 import edu.wwu.csci412.multipoll.Model.Category;
 import edu.wwu.csci412.multipoll.Model.Controller;
+import edu.wwu.csci412.multipoll.Model.Element;
 import edu.wwu.csci412.multipoll.Model.Group;
 import edu.wwu.csci412.multipoll.Model.User;
 import edu.wwu.csci412.multipoll.R;
 
+
 public class ChooseElements extends AppCompatActivity {
+    private ArrayAdapter<Element> arrayAdapter;
+    private EditText search;
 
     public static Controller controller;
     public static User user;
@@ -28,6 +36,13 @@ public class ChooseElements extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_elements);
+
+        /* Connect XML with Java objects */
+        search = findViewById(R.id.searchElement);
+        LinearLayout linearLayout = findViewById(R.id.elementContainer);
+        FloatingActionButton fab = findViewById(R.id.addElement);
+
+        /* Fake Database */
         controller = MainActivity.getController();
         user = controller.getUser();
 
@@ -36,50 +51,54 @@ public class ChooseElements extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        /* set up search view */
-        SearchView searchView = findViewById(R.id.search_elements);
-        searchView.setQueryHint("Search " + user.getCurrentPoll().getCategory());
-//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-//            @Override
-//            public boolean onQueryTextSubmit(String query) {
-//                return true;
-//            }
-//
-//            @Override
-//            public boolean onQueryTextChange(String newText) {
-//                getUsers(newText, new Callback() {
-//                    @Override
-//                    public void onCallback(Object value) {
-//                        restaurants = (ArrayList<RestaurantFirebase>) value;
-//                        restaurantAdapter.updateData(restaurants);
-//                    }
-//                });
-//
-//                return false;
-//            }
-//        });
-
-
-
-
-        /* list view of elements for current category */
-        ArrayAdapter<String> arrayAdapter;
-        ListView lv = findViewById(R.id.elements);
-        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, user.getCurrentPoll().listElements(user.getCurrentPoll().getElements()));
-        lv.setAdapter(arrayAdapter);
-
-        /* on click listener for list view buttons */
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /* Search view setup */
+        search.setHint("Search Elements");
+        search.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String TempListViewClickedValue = user.getCurrentPoll().listElements(user.getCurrentPoll().getElements()).get(position);
-                Intent intent = new Intent (ChooseElements.this, GroupSelected.class);
-//                user.setCurrentGroup(user.getGroup(TempListViewClickedValue));
-                startActivity(intent);
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                (ChooseElements.this).arrayAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
+
+        /* List of elements for current category */
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, user.getCurrentPoll().getElements());
+//        LinearLayout.LayoutParams buttonparam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT,1.0f);
+        for (int i = 0; i < arrayAdapter.getCount(); i++) {
+            Element element = arrayAdapter.getItem(i);
+            CheckBox checkBox = new CheckBox(this);
+            checkBox.setId(element.getId());
+            checkBox.setText(element.getName());
+//            checkBox.setLayoutParams(buttonparam);
+            checkBox.setWidth(linearLayout.getWidth());
+            linearLayout.addView(checkBox);
+        }
+
+        /* Floating Add Button Setup */
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Element newElement = new Element(search.getText().toString());
+                arrayAdapter.add(newElement);
+                user.getCurrentCategory().add(newElement);
+                arrayAdapter.notifyDataSetChanged();
+            }
+        });
+//        setContentView(linearLayout);
     }
-  
+
+//        public void onCheckboxClicked(View view){
+//        }
+//        }
+
     // Back functionality
     @Override public boolean onSupportNavigateUp() {
         onBackPressed();
